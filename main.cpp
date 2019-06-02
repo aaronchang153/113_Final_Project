21218e3577034ef5eb80c81b52f677717628a2a8
Aaron Chang <aaronchang153@gmail.com>
2019-06-01 19:04:07 -0700

    Added temporary main

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "cimis.h"
+
+
+// just a placeholder for now
+int main(){
+	struct CIMIS_data d;
+	get_latest_data(&d);
+	printf("%d,%s,%d,%d\n", d.station, d.date, d.hour, d.et0);
+	return 0;
+}
