58b1b74624eb1f2b60fda0a46de9f8f924fd8428
Cesar <c.alvarenga98@gmail.com>
2019-06-04 14:36:35 -0700

    adding temp and humidity files

---
+++ temp_and_humidity.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.c>
+
+float getTemp()
+{
+	float temp;
+
+
+	return temp;
+}
+
+
+float getHumidity()
+{
+	float humidity;
+
+	return humidity;
+}
---
+++ temp_and_humidity.h
@@ -0,0 +1,5 @@
+#ifndef TEMP_AND_HUMIDITY
+#define TEMP_AND_HUMIDITY
+
+float getTemp();
+float getHumidity();
