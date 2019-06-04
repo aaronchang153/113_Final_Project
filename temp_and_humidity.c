4f79b35423dbcb117dbcec0ffbcf32f912c739bb
Cesar <c.alvarenga98@gmail.com>
2019-06-04 15:07:42 -0700

    added code to temp_and_humidity files

--- temp_and_humidity.c
+++ temp_and_humidity.c
@@ -1,18 +1,66 @@
-#include <stdio.h>
-#include <stdlib.c>
+#include "temp_and_humidity.h"
 
 float getTemp()
 {
-	float temp;
+	DHT sensor;	//create a sensor object
+	int getSensor;	//read the value of the sensor
 
+		/*if initial wiring failed, print the error*/
+	if(wiringPiSetup() == -1)
+	{
+		printf("setup wiringPi failed\n");
+		return 1;
+	}	
 
-	return temp;
+	getSensor = sensor.readDHT11(DHT11_Pin);
+	switch(sensor)
+	{
+		case DHT_OK:		//everything is good
+			printf("Correct Value\n");
+			break;
+		case DHT_ERROR_CHECK:	//data check has errors
+			printf("DHT_ERROR_CHECK\n");
+			break;
+		case DHT_ERROR_TIMEOUT:	//reading DHT times out
+			printf("DHT_ERROR_TIMEOUT\n");
+			break;
+		case DHT_INVALID_VALUE	//other errors
+			printf("DHT_INVALID_VALUE");
+			break;
+	}
+
+	return sensor.temp;
 }
 
 
 float getHumidity()
 {
-	float humidity;
+	DHT sensor;     //create a sensor object
+        int getSensor;  //read the value of the sensor
 
-	return humidity;
+                /*if initial wiring failed, print the error*/
+        if(wiringPiSetup() == -1)
+        {
+                printf("setup wiringPi failed\n");
+                return 1;
+        }
+
+        getSensor = sensor.readDHT11(DHT11_Pin);
+        switch(sensor)
+        {
+                case DHT_OK:            //everything is good
+                        printf("Correct Value\n");
+                        break;
+                case DHT_ERROR_CHECK:   //data check has errors
+                        printf("DHT_ERROR_CHECK\n");
+                        break;
+                case DHT_ERROR_TIMEOUT: //reading DHT times out
+                        printf("DHT_ERROR_TIMEOUT\n");
+                        break;
+                case DHT_INVALID_VALUE  //other errors
+                        printf("DHT_INVALID_VALUE");
+                        break;
+        }
+
+        return sensor.humidity;
 }
