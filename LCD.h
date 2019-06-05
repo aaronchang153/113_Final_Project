004026e5a3756af18a8de093ebb2a576d5f5834c
Aaron Chang <aaronchang153@gmail.com>
2019-06-05 15:32:48 -0700

    Added some function declations

--- LCD.h
+++ LCD.h
@@ -1,1 +1,14 @@
-a
+#ifndef LCD_H
+#define LCD_H
+
+
+// setup pins and I2C stuff
+void setupLCD();
+
+// temperature in Fahrenheit, humidity in %, water_savings in gallons
+void lcdDisplayInfo(double local_temp, double local_humidity,
+					double, cimis_temp, double, cimis_humidity,
+					double local_et, double cimis_et,
+					double water_savings);
+
+#endif // LCD_H
