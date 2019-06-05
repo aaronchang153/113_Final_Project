10e9f9db527759cc1a1a1aece8e8a975f702e51e
Aaron Chang <aaronchang153@gmail.com>
2019-06-05 14:32:47 -0700

    Now gets temp and humidity directly from DHT functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <wiringPi.h>
 #include "cimis.h"
 #include "temp_and_humidity.h"
+#include "DHT.hpp"
 
 
 static double et_local[3];
@@ -9,7 +10,8 @@ static int et_index;
 static int et_count;
 
 
-void hourlyCheck(float temp, float humidity);
+void hourlyCheck(double temp, double humidity);
+void waterPlants();
 
 int main(){
 	if(wiringPiSetup() == -1){
@@ -22,8 +24,14 @@ int main(){
 	et_index = 0;
 	et_count = 0;
 
-	float avg_temp = 0.0f;
-	float avg_humidity = 0.0f;
+	DHT dht;
+
+	double avg_temp = 0.0l;
+	double avg_humidity = 0.0l;
+
+	// just a flag to know when the for loop is doing its first run
+	// used to properly calculate average temp+humidity
+	char first_run = 1;
 
 	// counts between 0 and 59, gets incremented eveytime local temp + humidity is checked
 	// used to track when an hour has passed
@@ -32,20 +40,46 @@ int main(){
 	for(;;){
 		counter = (counter + 1) % 60;
 
-		avg_temp += getTemp();
-		avg_humidity += getHumidity();
+		dht.readDHT11(DHT_PIN);
+
+		avg_temp += dht.temperature;
+		avg_humidity += dht.humidity;
+		
+		if(first_run){
+			first_run = 0;
+		}
+		else{
+			avg_temp /= 60.0l;
+			avg_humidity /= 60.0l;
+		}
+
+		hourlyCheck(avg_temp, avg_humidity);
+
+		waterPlants();
+
+		avg_temp = 0.0l;
+		avg_humidity = 0.0l;
+
+		delay(60000); // 1 minute delay
 	}
 
 	return 0;
 }
 
-void hourlyCheck(float temp, float humidity){
+void hourlyCheck(double temp, double humidity){
 	struct CIMIS_data cimis_data;
 
 	// if we successfully get the latest data from CIMIS
 	if(get_latest_data(&cimis_data) == 0){
-		double et = cimis_data.et0 * ((double) temp / cimis_data.air_temp) * (cimis_data.humidity / (double) humidity);
+		double et = cimis_data.et0 * (temp / cimis_data.air_temp) * (cimis_data.humidity / humidity);
 		et_index = (et_index + 1) % 3;
 		et_local[et_index] = et;
+		if(et_count < 3)
+			et_count++;
 	}
 }
+
+void waterPlants(){
+
+}
+
